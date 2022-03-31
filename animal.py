fb4b3f17e6ed465b21504b9f788d39c445dfcf0a
KaiGeller <gellerka@oregonstate.edu>
2022-03-30 21:19:09 -0700

    Project 1

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Kai Geller
+# GitHub username: KaiGeller
+# Date: 3/302022
+# Description: Ask the user for their favorite animal and then prints out that is their favorite animal
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
